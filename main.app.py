65e04ce83af6a13e6563ce7c929bedf6a5fa752f
Anton <antonafonin154@gmail.com>
2024-05-02 20:39:40 -0400

    finished project

--- main.app.py
+++ main.app.py
@@ -1,4 +1,5 @@
 import random
+from hangman import draw_hangman
 
 
 def random_word():
@@ -8,8 +9,45 @@ def random_word():
 
             return random.choice(words)
     except FileNotFoundError:
-        print('Error')
+        print('Error... File not found')
+
+
+def check_letter(letter):
+    return letter.isalpha() and len(letter) >= 0
+
+def game():
+    word = random_word().lower()
+    hidden_word = list('*' * len(word))
+    fail = 0
+    game_result = ''
+
+    print(word)
+
+    while True:
+        letter = input('Input a letter: ').lower()
+
+        if not check_letter(letter):
+            print('Please input correct letter -_- ')
+            continue
+
+        if letter in word:
+            for i in range(len(word)):
+                if word[i] == letter:
+                    hidden_word[i] = letter
+            print(''.join(hidden_word))
+        else:
+            print(draw_hangman(fail))
+            fail += 1
+
+        if '*' not in hidden_word:
+            game_result = 'Congratulation! You guessed the word'
+            break
+        elif fail == 7:
+            game_result = f'Sorry, you lost. The word was {word}'
+            break
+
+    return game_result
 
 
 if __name__ == '__main__':
-    print(random_word())
+    print(game())
